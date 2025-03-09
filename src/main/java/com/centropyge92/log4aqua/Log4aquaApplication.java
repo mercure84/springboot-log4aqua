8db15a6f17c2b95bdfa4f6ebf2eb8dc8b210a449
Julien Marcesse <julien.marcesse@gmail.com>
2025-03-09 22:04:51 +0100

    enable scheduling support in Log4aquaApplication

--- src/main/java/com/centropyge92/log4aqua/Log4aquaApplication.java
+++ src/main/java/com/centropyge92/log4aqua/Log4aquaApplication.java
@@ -2,8 +2,10 @@ package com.centropyge92.log4aqua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Log4aquaApplication {
 
 	public static void main(String[] args) {
